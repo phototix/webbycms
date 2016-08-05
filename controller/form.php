42bc259e393823ea3f2e6d973ba7e6cc0d5fb60b
phototix <phototix@hotmail.com>
2016-08-05 16:05:52 +0800

    Update form.php
    Form handler.

--- controller/form.php
+++ controller/form.php
@@ -5,39 +5,58 @@ if(isset($_GET["formset"])&&!empty($_GET["formset"])){
 	$formset=$_GET["formset"];
 }
 
-if($form<>""){
-	if($formset=="ajax"){
-		require("conn.php");
-		require("functions.php");
-		require("common.php");
-		$systemForm="controller/forms/".$form.".php";
+if($formset=="ajax"){
+	require("conn.php");
+	require("functions.php");
+	require("common.php");
+	if($form<>""){
+	$systemForm="forms/".$form.".php";
 		if(file_exists($systemForm)){
 			include($systemForm);
-		}else{
-			?><script>window.location="?<?=$form?>";</script><?
-		}
-	}else{
-		$systemForm="controller/forms/".$form.".php";
-		if(file_exists($systemForm)){
-			include($systemForm);
-		}else{
-			?><script>window.location="?<?=$form?>";</script><?
-		}
-	}
-}
 
-if($newInsertID<>""){
-	?><script>window.location="<?=$currentURl?>";</script><?
-}
-/* Error Message Delivery Function (Use Session) */
-if($systemSucces<>""){
-	if(isset($_SESSION["systemSucces"])){
-		$_SESSION["systemSucces"]=$systemSucces;	
+			if($newInsertID<>""){
+				?><script>window.location="<?=$currentURl?>";</script><?
+			}
+			/* Error Message Delivery Function (Use Session) */
+			if($systemSucces<>""){
+				if(isset($_SESSION["systemSucces"])){
+					$_SESSION["systemSucces"]=$systemSucces;	
+				}
+			}
+			if($systemError<>""){
+				if(isset($_SESSION["systemError"])){
+					$_SESSION["systemError"]=$systemError;	
+				}
+			}
+			
+		}else{
+			?><script>window.location="?<?=$form?>&<?=$formset?>";</script><?
+		}
 	}
-}
-if($systemError<>""){
-	if(isset($_SESSION["systemError"])){
-		$_SESSION["systemError"]=$systemError;	
+}else{
+	if($form<>""){
+		$systemForm="controller/forms/".$form.".php";
+		if(file_exists($systemForm)){
+			include($systemForm);
+
+			if($newInsertID<>""){
+				?><script>window.location="<?=$currentURl?>";</script><?
+			}
+			/* Error Message Delivery Function (Use Session) */
+			if($systemSucces<>""){
+				if(isset($_SESSION["systemSucces"])){
+					$_SESSION["systemSucces"]=$systemSucces;	
+				}
+			}
+			if($systemError<>""){
+				if(isset($_SESSION["systemError"])){
+					$_SESSION["systemError"]=$systemError;	
+				}
+			}
+
+		}else{
+			?><script>window.location="?<?=$form?>";</script><?
+		}
 	}
 }
 ?>
