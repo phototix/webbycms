6cd11e3b6cc3a5c7369bd3ad85ad9caa66478527
Brandon Chong Chan Yean <brandon@brandhex.com>
2016-09-22 16:57:30 +0800

    Implement with under construction module. - Change the demo module on/off at conn.php

---
+++ pages/demo/index.php
@@ -0,0 +1,11 @@
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="display-3">Hello, world with Demo Access!</h1>
+    <p>
+    This simple page template is for website development with underconstuction module on. You can change the design of the page via "/pages/demo/index.php"<br>
+    To view demo or preview of the website. Please login to admin panel to gain access of it.
+    </p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">This is button &raquo;</a></p>
+  </div>
+</div>
--- pages/index.php
+++ pages/index.php
@@ -6,19 +6,90 @@ Version: 2.1
 */
 
 // Template Default set include
-if($page<>""&&$page<>'index'){
-	$pageFileSystem="pages/".$page.'/index.php';
-	if(file_exists($pageFileSystem)){
-		include($pageFileSystem);
+
+if($systemDemo=="yes"){
+	
+	if($systemDemoStart=="yes"){
+		if($page<>""&&$page<>'index'){
+			$pageFileSystem="pages/".$page.'/index.php';
+			if(file_exists($pageFileSystem)){
+
+				include("pages/includes/layout/bodystart.php");
+				include($pageFileSystem);
+				include("pages/includes/layout/bodyend.php");
+
+			}else{
+				include('pages/error/404.php');
+			}
+		}else{
+			$pageFileSystem='pages/home/index.php';
+			if(file_exists($pageFileSystem)){
+				include("pages/includes/layout/bodystart.php");
+				include($pageFileSystem);
+				include("pages/includes/layout/bodyend.php");
+			}else{
+				include('pages/error/404.php');
+			}
+		}
 	}else{
-		include('pages/error/404.php');
+
+		if($systemWebLaunch==1){
+			if($page<>""&&$page<>'index'){
+				$pageFileSystem="pages/".$page.'/index.php';
+				if(file_exists($pageFileSystem)){
+					include("pages/includes/layout/bodystart.php");
+					include($pageFileSystem);
+					include("pages/includes/layout/bodyend.php");
+				}else{
+					include('pages/error/404.php');
+				}
+			}else{
+				$pageFileSystem='pages/home/index.php';
+				if(file_exists($pageFileSystem)){
+					include("pages/includes/layout/bodystart.php");
+					include($pageFileSystem);
+					include("pages/includes/layout/bodyend.php");
+				}else{
+					include('pages/error/404.php');
+				}
+			}
+		}else{
+			$pageFileSystem='pages/demo/index.php';
+			if(file_exists($pageFileSystem)){
+				include($pageFileSystem);
+			}else{
+				include('pages/error/404.php');
+			}
+		}
+
 	}
+
 }else{
-	$pageFileSystem='pages/home/index.php';
-	if(file_exists($pageFileSystem)){
-		include($pageFileSystem);
+
+	if($page<>""&&$page<>'index'){
+
+		$pageFileSystem="pages/".$page.'/index.php';
+		if(file_exists($pageFileSystem)){
+			include("pages/includes/layout/bodystart.php");
+			include($pageFileSystem);
+			include("pages/includes/layout/bodyend.php");
+		}else{
+			include('pages/error/404.php');
+		}
+
 	}else{
-		include('pages/error/404.php');
+		
+		$pageFileSystem='pages/home/index.php';
+		if(file_exists($pageFileSystem)){
+			include("pages/includes/layout/bodystart.php");
+			include($pageFileSystem);
+			include("pages/includes/layout/bodyend.php");
+		}else{
+			include('pages/error/404.php');
+		}
+
+
 	}
+
 }
 ?>
