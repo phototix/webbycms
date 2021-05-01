08dc0210a66cf08826293f48bee3659f61526917
brandonccy1982 <brandon@kkbuddy.com>
2021-05-01 19:05:11 +0800

    Add custom PHP Error Message on top for easy debugging.

--- controller/define.php
+++ controller/define.php
@@ -1,4 +1,11 @@
 <?php
+// Add Custom error handler function
+function customError($errno, $errstr, $errfile, $errline) {
+  echo '<div class="alert alert-danger"><p><b>Error:</b> ['.$errno.'] '.$errstr.' <br>at '.$errfile.': line-> '.$errline.'</div></p>';
+}
+// Return Error into function
+set_error_handler("customError");
+
 $row="";
 $systemError="";$systemSucces="";
 if(empty($_SESSION["systemError"])){
@@ -11,7 +18,7 @@ $page="";
 if(isset($_GET["page"])&&!empty($_GET["page"])){
 	$page=$_GET["page"];
 }
-if($page==""){
+if($page==""||$page=="index"){
 	$page="home";
 }
 $cate="";
