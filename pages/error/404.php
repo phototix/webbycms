66396669da72f8a4faa03355a8ef95e791bae55c
Brandon Chong <brandon@kkbuddy.com>
2023-03-03 10:23:35 +0800

    PHP page documentations

--- pages/error/404.php
+++ pages/error/404.php
@@ -1,1 +1,5 @@
+<?php
+// To show this page as a mock home page.
+// And PHP strings can be echo using <?=$string
+?>
 <h1>404 Page Not Found</h1>
--- pages/error/500.php
+++ pages/error/500.php
@@ -1,1 +1,5 @@
+<?php
+// To show this page as a mock home page.
+// And PHP strings can be echo using <?=$string
+?>
 <h1>500 Server Engine Not Found</h1>
