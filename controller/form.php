8e79e3310e4845debabc3da3a82f4740fc17d9e1
Amanda <64722792+amandachong1224@users.noreply.github.com>
2023-03-04 03:11:58 +0000

    Add documentation in form handling

--- controller/form.php
+++ controller/form.php
@@ -23,16 +23,20 @@ if(isset($_GET["redirectURL"])&&!empty($_GET["redirectURL"])){
 	$redirectURL=$_GET["redirectURL"];
 }
 
+// In AJAX mode
 if($formset=="ajax"){
 	session_start();
 	require("conn.php");
 	require("functions.php");
 	require("common.php");
 	if($form<>""){
+
+	// Load page's forms file to form action in AJAX mode.
 	$systemForm="pages/".$page."/forms/".$form.".php";
 		if(file_exists($systemForm)){
 			include($systemForm);
 
+			// Redirect to currentURL if mysql query have new insertID
 			if($newInsertID<>""){
 				?><script>window.location="<?=$currentURl?>";</script><?php
 			}
@@ -48,29 +52,39 @@ if($formset=="ajax"){
 				}
 			}
 
+			// Show loading text
 			if($showLoading==""){
 				?><h1>Loading...</h1><?php
+
+				// Redirect to given redirectURL if exsited. Else nothing
 				if(($redirectURL==""||$redirectURL<>"")&&$redirectURL<>"noredirect"){
 					if($redirectURL==""){ $redirectURL="/"; }
 					?><script>window.location="<?=$redirectURL?>";</script><?php
 				}elseif($redirectURL=="noredirect"){
-
+					// No redirection here.
 				}
 			}
 
 		}else{
+			// Return to Root Home and put missing form file in parameter.
 			?><script>window.location="?<?=$form?>&<?=$formset?>";</script><?php
 		}
 	}
+
+// In normal FORM post mode
 }else{
 	if($form<>""){
+
+		// Load page's forms file to form action.
 		$systemForm="pages/".$page."/forms/".$form.".php";
 		if(file_exists($systemForm)){
 			include($systemForm);
 
+			// Redirect to currentURL if mysql query have new insertID
 			if($newInsertID<>""){
 				?><script>window.location="<?=$currentURl?>";</script><?php
 			}
+
 			/* Error Message Delivery Function (Use Session) */
 			if($systemSucces<>""){
 				if(isset($_SESSION["systemSucces"])){
@@ -84,6 +98,7 @@ if($formset=="ajax"){
 			}
 
 		}else{
+			// Return to Root Home and put missing form file in parameter.
 			?><script>window.location="?<?=$form?>";</script><?php
 		}
 	}
